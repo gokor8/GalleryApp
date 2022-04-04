f8d33dc57b8307724fc8b4d0c2553e71415bf668
Gokor <gokor.paravyan@bk.ru>
2022-04-04 16:17:02 +0300

    Добавляю в BaseFragment поле с ViewModel

--- app/src/main/java/com/example/galleryapp/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/galleryapp/fragments/BaseFragment.kt
@@ -1,11 +1,13 @@
 package com.example.galleryapp.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 
-open class BaseFragment<F : ViewBinding> : Fragment() {
+open class BaseFragment<F : ViewBinding, V : ViewModel> : Fragment() {
 
     protected var binding: F? = null
+    protected var viewModel: V? = null
 
     override fun onDestroy() {
         binding = null
