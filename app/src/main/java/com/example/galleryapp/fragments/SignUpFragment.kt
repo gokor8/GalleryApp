f459a380d2a4607d8af9bba877efc33a7dd25946
Gokor <gokor.paravyan@bk.ru>
2022-04-04 18:13:34 +0300

    Прикидываю базовую логику валидаторов

--- app/src/main/java/com/example/galleryapp/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/galleryapp/fragments/SignUpFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.galleryapp.databinding.FragmentSignUpBinding
+import com.example.galleryapp.ValidationFactory
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding, SignUpFragmentViewModel>() {
 
@@ -15,10 +18,21 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding, SignUpFragmentViewMod
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this)[SignUpFragmentViewModel::class.java]
 
         binding?.apply {
             toolbarLayout.cancelTextView.setOnClickListener {
                 it.findNavController().popBackStack()
+            }
+
+            val emailEditText = nameInputLayout.editText as EditText
+            emailEditText.setOnFocusChangeListener { view, b ->
+                viewModel?.let {
+                    it.validate(
+                        emailEditText.toString(),
+                        ValidationFactory.ValidatorTypes.Email
+                    )
+                }
             }
 
             signUpButton.setOnClickListener {
--- app/src/main/java/com/example/galleryapp/fragments/SignUpFragmentViewModel.kt
+++ app/src/main/java/com/example/galleryapp/fragments/SignUpFragmentViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.galleryapp.fragments
 
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.example.galleryapp.ValidationFactory
+import com.example.galleryapp.ValidatorStates
 
-class SignUpFragmentViewModel : ViewModel(){
+class SignUpFragmentViewModel : ViewModel() {
 
-    fun trySignUp() : Boolean {
+    private val validatorFactory = ValidationFactory()
+    private val validateState = ValidatorStates(null, null)
+
+    fun validate(validateString: String, validatorType: ValidationFactory.ValidatorTypes) {
+        if (validateString.isNotEmpty())
+            validatorType = validatorFactory.create(validatorType).validate(validateString)
+    }
+
+    fun trySignUp(): Boolean {
         // some todo realm
         return false
     }
