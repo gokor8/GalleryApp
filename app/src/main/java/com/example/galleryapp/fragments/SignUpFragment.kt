2b50f3838264add8f81263d67b812e6a41de8e0e
Gokor <gokor.paravyan@bk.ru>
2022-04-07 19:25:03 +0300

    Меняю логику фрагмента и его vm под usecase и новые entity

--- app/src/main/java/com/example/galleryapp/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/galleryapp/fragments/SignUpFragment.kt
@@ -1,20 +1,27 @@
 package com.example.galleryapp.fragments
 
+import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.domain.entities.SignUpEntity
 import com.example.galleryapp.databinding.FragmentSignUpBinding
 import com.example.galleryapp.ValidationHandler
+import com.example.galleryapp.ui_ntities.UISignUpEntity
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignUpFragment : BaseFragment<FragmentSignUpBinding, SignUpFragmentViewModel>() {
+
+    private var datePickerDialog: DatePickerDialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,15 +41,20 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding, SignUpFragmentViewMod
                 val birthdayEditText = birthdayInputLayout.editText as TextInputEditText
 
                 vm.emailLiveData.observe(viewLifecycleOwner) {
-                    emailInputLayout.error = if(it==null) "" else getString(it)
+                    emailInputLayout.error = if (it == null) "" else getString(it)
                 }
 
                 vm.birthdayLiveData.observe(viewLifecycleOwner) {
-                    birthdayInputLayout.error = if(it==null) "" else getString(it)
+                    birthdayInputLayout.error = if (it == null) "" else getString(it)
+                }
+
+                vm.authViewModel.observe(viewLifecycleOwner) {
+                    if (it != null)
+                        Snackbar.make(root, it, Snackbar.LENGTH_SHORT).show()
                 }
 
                 emailEditText.setOnFocusChangeListener { _, isFocused ->
-                    if(isFocused) return@setOnFocusChangeListener
+                    if (isFocused) return@setOnFocusChangeListener
 
                     vm.validate(
                         emailEditText.text.toString(),
@@ -51,37 +63,45 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding, SignUpFragmentViewMod
                     )
                 }
 
-                birthdayEditText.setOnFocusChangeListener { _, isFocused ->
-                    if(isFocused) return@setOnFocusChangeListener
+                birthdayEditText.setOnClickListener {
+                    datePickerDialog?.let {
+                        if(it.isShowing)
+                            return@setOnClickListener
+                    }
 
-                    vm.validate(
-                        birthdayEditText.text.toString(),
-                        ValidationHandler.ValidationTypes.Date,
-                        vm.birthdayLiveData
-                    )
+                    context?.let { context ->
+                        datePickerDialog = DatePickerDialog(
+                            context
+                        ).apply {
+                            datePicker.maxDate = System.currentTimeMillis() - 1000
+                            datePicker.setOnDateChangedListener { _, year, mounth, day ->
+                                birthdayEditText.setText("$day.$mounth.$year")
+                            }
+                            show()
+                        }
+                    }
                 }
 
                 signUpButton.setOnClickListener {
                     val isNotEmpty = name.text!!.isNotEmpty()
                             && birthday.text!!.isNotEmpty()
-                        && email.text!!.isNotEmpty() && oldPassword.text!!.isNotEmpty() &&
-                        confirmPassword.text!!.isNotEmpty()
+                            && email.text!!.isNotEmpty() && oldPassword.text!!.isNotEmpty() &&
+                            confirmPassword.text!!.isNotEmpty()
 
-                    val withoutErrors =  name.error.isNullOrEmpty() && birthday.error.isNullOrEmpty()
+                    val withoutErrors = name.error.isNullOrEmpty() && birthday.error.isNullOrEmpty()
                             && email.error.isNullOrEmpty() && oldPassword.error.isNullOrEmpty() &&
                             confirmPassword.error.isNullOrEmpty()
 
-                    if(isNotEmpty && withoutErrors){
+                    if (isNotEmpty && withoutErrors) {
                         vm.trySignUp(
-                            SignUpEntity(
-                                name.text.toString(),
-                                confirmPassword.text.toString(),
-                                birthday.text.toString(),
-                                email.text.toString()
+                            UISignUpEntity(
+                                username = name.text.toString(),
+                                password = confirmPassword.text.toString(),
+                                birthday = birthday.text.toString(),
+                                email = email.text.toString()
                             )
                         )
-                    }
-                    else
+                    } else
                         signUpButton.setBackgroundColor(Color.RED)
                 }
             }
--- app/src/main/java/com/example/galleryapp/fragments/SignUpFragmentViewModel.kt
+++ app/src/main/java/com/example/galleryapp/fragments/SignUpFragmentViewModel.kt
@@ -3,20 +3,24 @@ package com.example.galleryapp.fragments
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.data.repository.UserRepository
 import com.example.domain.entities.SignUpEntity
+import com.example.domain.usecases.RegistrationUseCase
 import com.example.galleryapp.ValidationHandler
+import com.example.galleryapp.ui_ntities.UISignUpEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
 import javax.inject.Inject
 
 @HiltViewModel
-class SignUpFragmentViewModel @Inject constructor(private val repository: UserRepository) : ViewModel() {
+class SignUpFragmentViewModel @Inject constructor(
+    private val registrationUseCase: RegistrationUseCase) : ViewModel() {
 
     val emailLiveData = MutableLiveData<Int?>()
     val birthdayLiveData = MutableLiveData<Int?>()
-    val authViewModel = MutableLiveData<Boolean>()
+    val authViewModel = MutableLiveData<String>()
+
     private val validatorHandler = ValidationHandler()
 
     fun validate(
@@ -29,9 +33,25 @@ class SignUpFragmentViewModel @Inject constructor(private val repository: UserRe
                 .let(postLiveData::postValue)
     }
 
-    fun trySignUp(signUpEntity: SignUpEntity) {
+    fun trySignUp(uiSignUpEntity: UISignUpEntity) {
         viewModelScope.launch(Dispatchers.IO) {
-                repository.createUser(signUpEntity).let(authViewModel::postValue)
+            /*val sourceDateFormat = SimpleDateFormat("dd.MM.YYYY")
+            val srcBirthday = sourceDateFormat.parse(uiSignUpEntity.birthday)
+            val destBirthday = sourceDateFormat.format(srcBirthday)*/
+
+            val signUpEntity = SignUpEntity(
+                uiSignUpEntity.username,
+                uiSignUpEntity.password,
+                uiSignUpEntity.birthday,
+                uiSignUpEntity.email
+            )
+
+            val message = registrationUseCase.registrationUser(signUpEntity)
+            if (message == null) {
+                "Регистрация успешна".let(authViewModel::postValue)
+            } else {
+                message.let(authViewModel::postValue)
             }
+        }
     }
 }
