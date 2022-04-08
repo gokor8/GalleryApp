01faa889c291b9b37d2cf11669644db45ecf8c77
Gokor <gokor.paravyan@bk.ru>
2022-04-08 18:38:01 +0300

    Не значительный нейминг

--- domain/src/main/java/com/example/domain/repository/AuthorizationRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthorizationRepository.kt
@@ -1,11 +1,12 @@
 package com.example.domain.repository
 
+import com.example.domain.core.ValidationTypes
 import com.example.domain.entities.SignInEntity
 import com.example.domain.entities.SignUpEntity
 
 interface AuthorizationRepository {
 
-    suspend fun signUpUser(signUpEntity: SignUpEntity): String?
+    suspend fun signUpUser(signUpEntity: SignUpEntity): Map<ValidationTypes, String>?
 
-    suspend fun signInUser(signInEntity: SignInEntity): String?
+    suspend fun signInUser(signInEntity: SignInEntity): Map<ValidationTypes, String>?
 }
