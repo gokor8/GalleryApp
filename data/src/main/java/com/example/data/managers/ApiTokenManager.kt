bb3bc90b8ac02b704622afa94d2f2bc521fa5ea2
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-04-10 23:34:42 +0300

    Создаю Менеджер для удобной работы с клиент токеном

---
+++ data/src/main/java/com/example/data/managers/ApiTokenManager.kt
@@ -0,0 +1,28 @@
+package com.example.data.managers
+
+import com.example.data.datasource.CloudTokenDataSource
+import com.example.data.datasource.SharedPreferencesDataSource
+import com.example.data.storages.KeysEntity
+import javax.inject.Inject
+
+class ApiTokenManager @Inject constructor(
+    private val cloudTokenDataSource: CloudTokenDataSource,
+    private val sharedPreferencesDataSource: SharedPreferencesDataSource
+) {
+
+    suspend fun getFreshToken(): KeysEntity {
+        var token = sharedPreferencesDataSource.getKeys()
+        if(token.clientId == null)
+        {
+            token = cloudTokenDataSource.getNewToken().mapTo()
+            sharedPreferencesDataSource.saveKeys(token)
+        } else {
+            val apiTokens = cloudTokenDataSource.getAvailableToken(token.clientId!!).mapTo()
+            if(apiTokens != token)
+                sharedPreferencesDataSource.saveKeys(apiTokens)
+            token = apiTokens
+        }
+
+        return token
+    }
+}
