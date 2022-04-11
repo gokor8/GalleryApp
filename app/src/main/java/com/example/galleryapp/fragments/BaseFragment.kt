a88d16798cefc2ad6309dfd532b824c1f27f2319
Gokor <gokor.paravyan@bk.ru>
2022-04-11 23:08:55 +0300

    Добавляю шаблонный метод в basefragment

--- app/src/main/java/com/example/galleryapp/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/galleryapp/fragments/BaseFragment.kt
@@ -3,10 +3,11 @@ package com.example.galleryapp.fragments
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.textfield.TextInputLayout
 
-open class BaseFragment<F : ViewBinding, V : ViewModel> : Fragment() {
+open class BaseFragment<B : ViewBinding, V : ViewModel> : Fragment() {
 
-    protected var binding: F? = null
+    protected var binding: B? = null
     protected var viewModel: V? = null
 
     override fun onDestroy() {
@@ -14,4 +15,10 @@ open class BaseFragment<F : ViewBinding, V : ViewModel> : Fragment() {
         super.onDestroy()
     }
 
+    protected fun checkErrorTextInputLayout(textInputLayout: TextInputLayout, error: String){
+        textInputLayout.error = error
+
+        if(error == "")
+            textInputLayout.isErrorEnabled = false
+    }
 }
