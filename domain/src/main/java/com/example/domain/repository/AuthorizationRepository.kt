90f47cd9b491d6327355eb5bf5fccc01f37d2912
Gokor <gokor.paravyan@bk.ru>
2022-04-15 17:10:58 +0300

    Исправляю нейминг

--- domain/src/main/java/com/example/domain/repository/AuthorizationRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthorizationRepository.kt
@@ -1,6 +1,5 @@
 package com.example.domain.repository
 
-import com.example.domain.core.ValidationTypes
 import com.example.domain.entities.AuthState
 import com.example.domain.entities.SignInEntity
 import com.example.domain.entities.SignUpEntity
