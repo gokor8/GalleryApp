429b29c476266c336470fe93708380671fb3cd89
Gokor <gokor.paravyan@bk.ru>
2022-04-15 17:21:20 +0300

    Переношу логику проверки возвращаемого статуса в менеджер ответов ретрофита(RetrofitResponseManager

--- data/src/main/java/com/example/data/repository/UserAuthorizationRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/UserAuthorizationRepositoryImpl.kt
@@ -1,10 +1,7 @@
 package com.example.data.repository
 
 import com.example.data.datasource.CloudAuthDataSource
-import com.example.data.datasource.CloudTokenDataSource
 import com.example.data.managers.ApiTokenManager
-import com.example.data.storages.CacheService
-import com.example.domain.core.ValidationTypes
 import com.example.domain.entities.AuthState
 import com.example.domain.entities.SignInEntity
 import com.example.domain.entities.SignUpEntity
@@ -20,17 +17,12 @@ class UserAuthorizationRepositoryImpl @Inject constructor(
 
     override suspend fun signUpUser(signUpEntity: SignUpEntity): AuthState {
         return withContext(Dispatchers.IO) {
-            val responseData = cloudAuthDataSource.signUpUser(signUpEntity)
-
-            return@withContext if (responseData != null)
-                AuthState.Error(responseData)
-            else
-                AuthState.Success(true)
+            return@withContext cloudAuthDataSource.signUpUser(signUpEntity)
         }
     }
 
     override suspend fun signInUser(signInEntity: SignInEntity): AuthState {
 
-        return AuthState.Error("Я ничего не написал тут, поэтому ошибка")//authCloudDataSource.signInUser(signInEntity)
+        return AuthState.Exception("Я ничего не написал тут, поэтому ошибка")//authCloudDataSource.signInUser(signInEntity)
     }
 }
