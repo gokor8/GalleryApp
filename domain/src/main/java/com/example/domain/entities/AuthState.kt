e2f9732748b7494d55ff7aaabdf8bf438a7df2d2
Gokor <gokor.paravyan@bk.ru>
2022-04-15 17:11:48 +0300

    Меняю хранящиеся данные в классах состояния авторизации

--- domain/src/main/java/com/example/domain/entities/AuthState.kt
+++ domain/src/main/java/com/example/domain/entities/AuthState.kt
@@ -1,8 +1,10 @@
 package com.example.domain.entities
 
+import com.example.domain.core.ErrorType
+
 sealed class AuthState{
 
     class Success(val isSuccess: Boolean) : AuthState()
-    class Error(val error: String) : AuthState()
-    class Exception() : AuthState()
+    class Error(val errorMap: Map<ErrorType, String>) : AuthState()
+    class Exception(val errorMessage: String) : AuthState()
 }
