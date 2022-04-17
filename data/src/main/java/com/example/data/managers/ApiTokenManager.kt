44fc3db08abb884daf4e9741284d3e8e9be14203
CiganVSeti@bk.ru <gvalal@bk.ru>
2022-04-17 23:05:46 +0300

    Примерная логика с запросом

--- data/src/main/java/com/example/data/managers/ApiTokenManager.kt
+++ data/src/main/java/com/example/data/managers/ApiTokenManager.kt
@@ -3,12 +3,25 @@ package com.example.data.managers
 import com.example.data.datasource.ApiTokenDataSource
 import com.example.data.datasource.SharedPreferencesDataSource
 import com.example.data.storages.KeysEntity
+import retrofit2.Response
 import javax.inject.Inject
 
 class ApiTokenManager @Inject constructor(
     private val apiTokenDataSource: ApiTokenDataSource,
     private val sharedPreferencesDataSource: SharedPreferencesDataSource
 ) {
+
+    suspend inline fun<R> sendWithToken(request: (tokens: KeysEntity) -> Response<R>): Response<R> {
+        var tokens = getSavedToken()
+        val response = request(tokens)
+
+        if(response.code() == 400) {
+            tokens = refreshToken()
+            return request(tokens)
+        }
+
+        return response
+    }
 
     suspend fun getSavedToken(): KeysEntity {
         var token = sharedPreferencesDataSource.getKeys()
